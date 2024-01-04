3429c662a97c0e12a00357b439c2634ac5970916
Eduardo Luiz <hello@eduardoluiz.dev>
2024-01-04 14:44:25 -0300

    feat: initial setup 👷

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,9 @@
+import { UserSignOutButton } from '@/components/user-sign-out-button'
+
+export default function Dashboard() {
+  return (
+    <h1>
+      dashboard123 <UserSignOutButton />
+    </h1>
+  )
+}
