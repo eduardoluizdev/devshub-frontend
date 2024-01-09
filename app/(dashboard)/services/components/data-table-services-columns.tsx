0a116b85a412490e38cca27681d9cbb6ed8e9b42
Eduardo Luiz <hello@eduardoluiz.dev>
2024-01-09 01:09:28 -0300

    feat: add customer name column in service list

--- app/(dashboard)/services/components/data-table-services-columns.tsx
+++ app/(dashboard)/services/components/data-table-services-columns.tsx
@@ -4,7 +4,13 @@ import { ColumnDef } from '@tanstack/react-table'
 
 import { Service } from '@/entities/service'
 
-const dataTableCustomersColumns: ColumnDef<Service>[] = [
+type ServiceWithCustomer = Service & {
+  customer: {
+    name: string
+  }
+}
+
+const dataTableCustomersColumns: ColumnDef<ServiceWithCustomer>[] = [
   {
     accessorKey: 'name',
     header: 'Nome',
@@ -16,6 +22,23 @@ const dataTableCustomersColumns: ColumnDef<Service>[] = [
   {
     accessorKey: 'renewal',
     header: 'Renovação',
+  },
+  {
+    accessorKey: 'customer',
+    header: 'Clientes',
+    cell: ({ row }) => {
+      const customer = row.original.customer ? row.original.customer : null
+
+      return (
+        <div className="flex gap-2">
+          {customer && (
+            <span className="bg-muted-foreground p-1 rounded text-primary-foreground">
+              {customer.name}
+            </span>
+          )}
+        </div>
+      )
+    },
   },
 ]
 
